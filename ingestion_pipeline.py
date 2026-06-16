3a355a818a61ab81a138f10f58d741e44627557b
Krishnendu Narayan <57310676+Krishnenu@users.noreply.github.com>
2026-06-17 01:18:52 +0530

    initial setup

---
+++ ingestion_pipeline.py
@@ -0,0 +1,15 @@
+import os
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print("main function")
+
+if __name__ == "__main__":
+    main()
